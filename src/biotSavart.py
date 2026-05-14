5b4c44de3981d99d3f363bb3ae27c84825e0f523
DaniyalKhanGit <daniyalkhan8741@gmail.com>
2026-05-14 09:35:18 -0400

    WoS start

--- src/biotSavart.py
+++ src/biotSavart.py
@@ -17,7 +17,10 @@ def biotSavartSolver(x: np.array[2], time: int, n_samples: int) -> int:
         if (time == 0):
             nth_vorticity = initVor(y_i[0], y_i[1])
         else:
-            nth_vorticity = recursiveSolver(y_i, (time - time_step), n_samples)
+            if (cacheFetch(x, time) != 0):
+                nth_vorticity = cacheFetch(x, time)
+            else:
+                nth_vorticity = recursiveSolver(y_i, (time - time_step), n_samples)
 
         diff = x - y_i
         G = diff / (2*mt.pi*diff^2) + mt.exp(-100)
@@ -25,7 +28,7 @@ def biotSavartSolver(x: np.array[2], time: int, n_samples: int) -> int:
         summation += crossprod
     return summation / n_samples
 
-def recursiveSolver(x: np.array[2], time: int, n_samples: int) -> np.array[2]:
+def recursiveSolver(x: np.array[1], time: int, n_samples: int) -> np.array[1]:
     ntime = time - time_step
     if (time == 0):
         return initVor(x[0], x[1])
@@ -37,7 +40,7 @@ def recursiveSolver(x: np.array[2], time: int, n_samples: int) -> np.array[2]:
 # caching handled here
 # return of 0 means unsuccessful, anything else is good
 
-def cachingSolver(x: np.array[2], time: int) -> int:
+def cachingSolver(x: np.array[1], time: int) -> int:
     
     if (cache[x[0]][x[1]] == 0):
         cache[x[0]][x[1]] = [x, time]
@@ -50,4 +53,29 @@ def cachingSolver(x: np.array[2], time: int) -> int:
     return 0
 
 
-def cachingHelper(x, time) -> np.array[any]:
+def nearestCoord(x) -> np.array[1]:
+    
+    if (x[0] % 1 >= 0.5):
+        nearestX = mt.ceil(x[0])
+    else:
+        nearestX = mt.floor(x[0])
+
+    if (x[1] % 1 >= 0.5):
+        nearestY = mt.ceil(x[1])
+    else:
+        nearestY = mt.floor(x[1])
+
+    if (nearestX >= 64): nearestX = 63
+    elif (nearestX < 0): nearestX = 0
+
+    if (nearestY >= 64): nearestY = 63
+    elif (nearestY < 0): nearestY = 0
+
+    nearCoord = np.array(nearestX, nearestY)
+    return nearCoord
+
+
+def cacheFetch(x, time) -> np.array[1]:
+    
+    gridPos = nearestCoord(x)
+    return cache[gridPos[0],gridPos[1]]
